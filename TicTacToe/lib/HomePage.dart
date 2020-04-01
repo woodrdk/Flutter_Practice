a5a41753ec1b3ce09ca2e6755bca45d5a32bc936
Robert Wood Jr <rdrwood@gmail.com>
2020-03-31 18:48:08 -0700

    working on setup

--- TicTacToe/lib/HomePage.dart
+++ TicTacToe/lib/HomePage.dart
@@ -9,6 +9,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  
+  
+  // link up images
+  AssetImage cross = AssetImage("images/cross.png");
+  AssetImage circle = AssetImage("images/circle.png");
+  AssetImage edit = AssetImage("images/edit.png");
+
+  bool isCross = true;
+  String message;
+  List<String> gameState;
+
+  // initialize the state of the box with empty
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      this.gameState = [
+        "empty","empty","empty","empty","empty","empty","empty","empty","empty",
+      ];
+      this.message = "";
+    });
+  }
+
+  //TODO: play game method
+
+
+  //TODO: reset game method
+
+
+  //TODO: get image method
+
+
+  //TODO: check for win logic
+  
+  
   @override
   Widget build(BuildContext context) {
     return Container(
