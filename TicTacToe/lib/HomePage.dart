2b3b0c1de835a5dd977526ce7a64905be462a570
Robert Wood Jr <rdrwood@gmail.com>
2020-03-31 21:38:39 -0700

    game board made

--- TicTacToe/lib/HomePage.dart
+++ TicTacToe/lib/HomePage.dart
@@ -141,8 +141,41 @@ class _HomePageState extends State<HomePage> {
   
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tic Tac Toe'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+              child: GridView.builder(
+                padding: EdgeInsets.all(20.0),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.0,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 10.0
+                ),
+                itemCount: this.gameState.length,
+                itemBuilder: (context, i) => SizedBox(
+                  width: 100.0,
+                  height: 100.0,
+                  child: MaterialButton(
+                    onPressed: (){
+                      playGame(i);
+                    },
+                    child: Image(
+                      image: this.getImage(this.gameState[i]),
+                    ),
+                  ),
+                ),
+              ),
+          ),
+          
+        ],
+      ),
     );
   }
 }
