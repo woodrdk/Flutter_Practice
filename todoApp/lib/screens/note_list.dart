f14436742830f9cb9e3da893c42af9b3b86e2f36
Robert Wood Jr <rdrwood@gmail.com>
2020-04-03 12:26:21 -0700

    to do set up

---
+++ todoApp/lib/screens/note_list.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class NoteList extends StatefulWidget {
+  NoteList({Key key}) : super(key: key);
+
+  @override
+  _NoteListState createState() => _NoteListState();
+}
+
+class _NoteListState extends State<NoteList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       
+    );
+  }
+}
