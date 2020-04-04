8d87e446fa08da38eb8e5db6773ecff1ff934dad
Robert Wood Jr <rdrwood@gmail.com>
2020-04-03 19:30:35 -0700

    created semi list screen code

---
+++ todoApp/lib/screens/note_details.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NoteDetail extends StatefulWidget {
+  
+  @override
+  _NoteDetailState createState() => _NoteDetailState();
+
+}
+
+class _NoteDetailState extends State<NoteDetail> {
+  
+  Widget build(BuildContext context) {
+    return Container(
+       child: child,
+    );
+  }
+}
--- todoApp/lib/screens/note_list.dart
+++ todoApp/lib/screens/note_list.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import '../database_helper.dart';
+import '../Note.dart';
+import 'note_details.dart';
+import 'package:sqflite/sqflite.dart';
 
 
 class NoteList extends StatefulWidget {
@@ -9,10 +14,43 @@ class NoteList extends StatefulWidget {
 }
 
 class _NoteListState extends State<NoteList> {
+  DatabaseHelper databaseHelper = DatabaseHelper()
+  List<Note> noteList;
+  int count = 0;
+
+
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-       
+    return Scaffold(
+       appBar: AppBar(
+         title: Text('LCO ToDo'),
+         backgroundColor: Colors.purple,
+       ),
+       body: Container(
+
+       ),
+       floatingActionButton: FloatingActionButton(
+         backgroundColor: Colors.purple,
+         child: Icon(Icons.add),
+         onPressed: (){
+           navigateToDetail(Note('', '', 2),'Add Note');
+         },
+       ),
     );
+  }
+
+
+  void navigateToDetail(Note note, String title) async {
+    bool result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) {
+          return NoteDetail(note, title);
+    }));
+
+    if(result == true){
+      //TODO update the view
+    }
   }
 }
